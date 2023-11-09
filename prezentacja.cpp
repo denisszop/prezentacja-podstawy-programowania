18cc52d081676ac35e6bf56106d0ce60a929a1a6
denisszop <101829514+denisszop@users.noreply.github.com>
2023-11-09 10:37:09 +0100

    Add files via upload

---
+++ prezentacja.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+//nie trzeba poprzedzac cin cout specyfikatorem std::
+#include <fstream>
+#include <string>
+//************************
+int main()
+{
+    string plikA;
+    string plikB;
+    //--------------------
+    cout << " Podaj nazwe pliku wejsciowego: ";
+    // odpowiednik printf() dla C
+    cin >> plikA;
+    // odpowiednik scanf() dla C
+    ifstream czyt(plikA.c_str());
+    //definicja obiektu klasy ifstream (strumien wejsciowy)
+    
+    if(!czyt)
+    {
+        cout << " Nie moge otworzyc tego pliku";
+        return 1;
+    }
+    
+    //--------------------
+    
+    cout << " Podaj nazwe pliku wyjsciowego: ";
+    cin >> plikB;
+    ofstream pisz(plikB.c_str());
+    //definicja obiektu klasy ofstream (strumien wyjsciowy)
+    
+    if(!pisz)
+    {
+        cout << " Nie moge otworzyc tego pliku";
+        return 1;
+    }
+    
+    // //---przepisywanie---
+    // char c;
+    // do
+    // {
+    //     czyt.get(c);
+    //     pisz.put(c);
+    // }
+    // while(!czyt.eof());
+    //     //----sprawdzanie----
+    //     if(czyt.eof())
+    //         cout << "\n Przepisano z "
+    //         << plikA
+    //         << " do "
+    //         << plikB
+    //         << ".\n";
+    //     else
+    //         cout << " Blad czytania\n";
+
+    //---przepisywanie---
+    string c;
+    do
+    {
+        getline(czyt, c);
+        if (!czyt.eof())
+            pisz << c << endl;
+        else
+            pisz << c;
+    }
+    while(!czyt.eof());
+        //----sprawdzanie---- 
+        if(czyt.eof()) { // ???
+            cout << "\n Przepisano z " << plikA << " do " << plikB << ".\n";
+        }
+        else
+            cout << " Blad czytania\n";
+        
+    
+    return 0;
+}
